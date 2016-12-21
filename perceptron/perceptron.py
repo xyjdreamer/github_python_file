41a9166f69c7f2f375dfe6b799122cc169d1d327
Administrator <xyj_dreamer@163.com>
2016-12-22 00:18:58 +0800

    first

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -1,44 +1,55 @@
-import os
+#!usr/bin/env python
+#-*- coding: utf-8 -*-
+
 import numpy as np
 
-#get data and augmented matrix
-dataSet = []
-file = open('data.txt')
-for line in file.readlines():  
-    lineArr = line.strip().split('\t')
-    if int(lineArr[3]) == 1:  
-        dataSet.append([float(lineArr[0]), float(lineArr[1]), float(lineArr[2]), float(1)])  
-    if int(lineArr[3]) == -1:
-        dataSet.append([(-1)*float(lineArr[0]), (-1)*float(lineArr[1]), (-1)*float(lineArr[2]), float(-1)])
-##print dataSet
+'''
+单层感知器
+'''
+class perceptron():
+    def __init__(self):
+        pass
+    #label = -1, +1
+    def train(self,input,label,learning_rate=0.1,epochs=5000):
+        #增加偏置
+        input_bias = np.hstack((input,np.ones((input.shape[0],1))))
+        #初始化权值,列向量
+        self.weight = 2 * np.random.random((input_bias.shape[1],1)) - 1
+        iter = 0
+        p = np.dot(input_bias,self.weight)
+        temp = label*p.reshape((1,-1))
+        temp[temp>0] = 0
+        temp[temp<=0] = 1
+        while iter < epochs and np.sum(temp)>0:
+            iter += 1
+            index = np.random.randint(input_bias.shape[0])
+            X = input_bias[index]
+            Y = np.dot(X,self.weight)
+            if label[index]*Y <= 0: # 如果预测结果有错,更新权值
+                self.weight  = self.weight + learning_rate*Y*label[index]
 
-#initialize w[]
-w = [0,0,0,0]
-
-#begin the iteration
-flag = False
-count = 0
-while(flag or count == 0):
-    count += 1
-    flag = False
-    for i in xrange(len(dataSet)):
-        label = 0
-        temp = dataSet[i]
-        for j in xrange(len(w)):
-            label += w[j]*temp[j]
-        if(label > 0):
-            print(w)
-        else:
-            print("w is changed:")
-            for k in xrange(len(w)):
-                w[k] += temp[k]
-            print(w)
-            flag = True
-print("the count of iteration is " + repr(count))
-print("the final w is:")
-print(w)
+            p = np.dot(input_bias, self.weight)
+            temp = label * p.reshape((1, -1))
+            temp[temp > 0] = 0
+            temp[temp <= 0] = 1
+        print temp
 
 
+    def sigmoid(self,x):
+        return 1.0/(1+np.exp(x))
+
+    def predict(self,data):
+        data_bias = np.hstack((data,np.ones(1)))
+        return np.dot(data_bias,self.weight)
+
+if __name__=='__main__':
+    data1 = np.array([[0,0],[0,1],[1,0],[1,1]])
+    data2 = np.array([0,1,1,1])
+
+    nn = perceptron()
+    nn.train(data1,data2)
+    for test in data1:
+        print nn.predict(test)
 
 
 
